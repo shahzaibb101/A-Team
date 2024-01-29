25f2f66448557d8afd257d4d43549fbf67417653
dahongj <jiangdahong2001@gmail.com>
2024-01-28 22:51:54 -0500

    Added fields for user and tasks

--- tables.sql
+++ tables.sql
@@ -1,10 +1,12 @@
 CREATE TABLE user(
-    id int,
+    id int PRIMARY KEY,
     LastName varchar(255),
     FirstName varchar(255),
-    
+    username varchar(255),
+    password varchar(255)
 );
 
 CREATE TABLE tasks(
-
-)
+    task_name varchar(255),
+    task_desc varchar(255)
+);
