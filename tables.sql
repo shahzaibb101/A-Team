04fbe67fa11505267a4bd4d7bde699a550947c5e
dahongj <jiagndahong2001@gmail.com>
2024-01-31 13:05:49 -0500

    Edited tasks table

--- tables.sql
+++ tables.sql
@@ -1,13 +1,18 @@
-CREATE TABLE user(
+CREATE TABLE User(
     email varchar(50),
-    last_name varchar(50)NOT NULL,
-    first_name varchar(50)NOT NULL,
-    username varchar(50)NOT NULL,
+    last_name varchar(50) NOT NULL,
+    first_name varchar(50) NOT NULL,
+    username varchar(50) NOT NULL,
     password varchar(32) NOT NULL,
-    primary key (email)
+    primary key(email)
 );
 
-CREATE TABLE tasks(
-    task_name varchar(255),
-    task_desc varchar(255)
+CREATE TABLE Tasks(
+    task_id int,
+    email varchar(50),
+    task_name varchar(50) NOT NULL,
+    task_desc varchar(255) NOT NULL,
+    foreign key(email) references User(email),
+    primary key(task_id,email)
 );
+
