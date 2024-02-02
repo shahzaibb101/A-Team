5dacfe94e62f39079a4187a5580fe31b53654241
Mark Deng <90063806+MarkDeng86@users.noreply.github.com>
2024-02-02 01:06:29 -0500

    Changed Database in settings.py
    Changed configuration of database host in settings.py to local host mysql.

--- ateam/settings.py
+++ ateam/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = "ateam.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'A_Team_DB',
+        'USER': 'root',
+        'PASSWORD': 'abc123',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
