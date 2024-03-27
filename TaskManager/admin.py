15ee46cc5473f9428031ce819ee31200f1b0695e
Dahong Jiang <dahongjiang2001@hotmail.com>
2024-03-27 13:49:45 -0400

    requirements

--- TaskManager/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User,Tasks, Feedback, Work_Session
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Tasks)
-admin.site.register(Feedback)
-admin.site.register(Work_Session)
