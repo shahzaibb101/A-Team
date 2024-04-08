c234e44cf54bcde387924a1b2ff3f6f2db8073e5
Mark Deng <90063806+MarkDeng86@users.noreply.github.com>
2024-04-08 14:20:17 -0400

    Tasklist validation
    Add uniqueness for combination of task and deadline

--- todo/forms.py
+++ todo/forms.py
@@ -10,3 +10,12 @@ class TaskForm(forms.ModelForm):
             'task_Assign_Date': forms.DateInput(attrs={'type': 'date'}),
             'deadline': forms.DateInput(attrs={'type': 'date'}),
         }
+	def clean(self):
+		cleaned_data = super().clean()
+		task_name = cleaned_data.get('task')
+		due_date = cleaned_data.get('deadline')
+
+		if TaskList.objects.filter(task=task_name, deadline=due_date).exists():
+			raise forms.ValidationError('The combination of Task and Deadline already exists.')
+
+		return cleaned_data
